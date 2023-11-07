0091c93be07f6654c666c94eeaf6c07e14240bcc
h-pk-11-windows <105375102+h-pk-11@users.noreply.github.com>
2023-11-07 11:38:29 +0700

    add webp format

--- webpack.common.js
+++ webpack.common.js
@@ -31,7 +31,7 @@ module.exports = {
                 use: ['style-loader', 'css-loader'],
             },
             {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
                 type: 'asset/resource',
                 generator: {
                     filename: '../assets/images/[hash][ext][query]',
